ab7f074a94d5062fdad83e74987c9e17e69b3e50
KiNG <odon66674@gmail.com>
2024-06-26 17:35:22 +0200

    first commit

--- app/Components/Contact.tsx
+++ app/Components/Contact.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <section className='mt-[6rem] mb-[10rem] ' >
+    <section className='overflow-hidden relative mt-[6rem] mb-[10rem] ' >
         <div className="containe w-full">
-            <div className="flex justify-center items-center flex-col rounded-3xl bg-cyan-50 p-10 ">
-                <h1 className='text-[25px] lg:text-[45px] md:text-[35px] text-center mb-5 max-w-[750px] font-semibold ' >Prepared to turn your ideas into reality? I'm here to help </h1>
+          <div className="absolute w-[10rem] h-[10rem] bg-white rounded-full opacity-30 -left-10 " />
+          <div className="absolute w-[10rem] h-[10rem] bg-white rounded-full opacity-30 left-[1rem] top-[7rem]  " />
+            <div className="flex justify-center items-center flex-col rounded-3xl bg-gradient-to-r from-cyan-300 to-teal-500 p-10 ">
+                <h1 className='text-[25px] lg:text-[45px] md:text-[35px] text-center mb-5 max-w-[750px] font-semibold bg-clip-text text-transparent bg-gradient-to-r from-black to-slate-900 ' >Prepared to turn your ideas into reality? I'm here to help </h1>
                 <Link href={'contact'} >
-                    <button className='py-3 px-4 font-semibold text-white hover:bg-[var(--ap)] bg-cyan-600 transition-all rounded-full  ' >Contact me</button>
+                    <button className='py-3 px-4 font-semibold text-white hover:bg-sky-800 bg-sky-600 transition-all rounded-xl  ' >Contact me</button>
                 </Link>
             </div>
         </div>
--- app/Components/ProjectCard.tsx
+++ app/Components/ProjectCard.tsx
@@ -7,8 +7,8 @@ const ProjectCard = ({item}:any) => {
     <Card className="max-w-[400px] shadow-lg border-[1px] relative rounded-2xl ">
       <CardHeader className="flex group max-h-[400px] relative flex-col cursor-pointer bg-orange-50 gap-3 overflow-hidden">
       <div className="absolute top-[40%] left-[35%] flex z-50 gap-4">
-          <Link href={item.link} className="bg-slate-900 hover:text-[var(--main)] text-white w-[54px] h-[54px] flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 rounded-full gap-2 transition-all duration-200 "> <Link2Icon  />  </Link>
-          <Link href={item.link} className="bg-slate-900 hover:text-[var(--main)] text-white w-[54px] h-[54px] flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 rounded-full gap-2 transition-all duration-200 "> <Github  />  </Link>
+          <Link href={"https://github.com/king-sws?tab=repositories"} className="bg-slate-900 hover:text-[var(--main)] text-white w-[54px] h-[54px] flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 rounded-full gap-2 transition-all duration-200 "> <Link2Icon  />  </Link>
+          <Link href={"https://github.com/king-sws?tab=repositories"} className="bg-slate-900 hover:text-[var(--main)] text-white w-[54px] h-[54px] flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 rounded-full gap-2 transition-all duration-200 "> <Github  />  </Link>
         </div>
         <p className='absolute top-2 left-2 capitalize text-[10px] font-bold bg-[var(--main)] px-2 rounded-full text-white ' > {item.cartegory} </p>
         <img alt="nextui logo" width={247} height={250} src={item.image} className='  shadow-2xl opacity-100 z-40 ' />
